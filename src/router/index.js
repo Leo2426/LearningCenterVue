ccccc94ddfae0d8eb5523f6b7a715d6fd3828138
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-11 19:58:59 -0500

    feat(routing): configured initial static and dynamic routes.

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// Routing Module
+
+import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+
+const AboutComponent = () => import('../public/pages/about.component.vue');
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/home',    component: HomeComponent },
+        { path: '/',        redirect: '/home' },
+        { path: '/about',   component: AboutComponent }
+    ]
+})

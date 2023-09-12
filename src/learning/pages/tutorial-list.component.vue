dcf4624dbaef56ef0244f0b4daa397972ff8e5f5
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-11 20:55:53 -0500

    feat(tutorials): added initial configuration for component.

---
+++ src/learning/pages/tutorial-list.component.vue
@@ -0,0 +1,42 @@
+<template>
+
+</template>
+
+<script>
+import {TutorialsApiService} from "../services/tutorials-api.service.js";
+
+export default {
+  name: "tutorial-list",
+  title: "Tutorials",
+  data() {
+    return {
+      tutorials: [],
+      tutorialDialog: false,
+      deleteTutorialDialog: false,
+      deleteTutorialsDialog: false,
+      tutorial: {},
+      selectedTutorials: [],
+      filters: {},
+      submitted: false,
+      statuses: [
+        { label: "Published", value: "published" },
+        { label: "Unpublished", value: "unpublished" },
+      ],
+      tutorialsService: null,
+    }
+  },
+  created() {
+    console.log('created');
+    this.tutorialsService = new TutorialsApiService();
+    this.tutorialsService.getAll()
+        .then(response => {
+          this.tutorials = response.data;
+          console.log(response.data);
+        })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

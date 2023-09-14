3da6c8f9dce304145ea969fd82226040cfda27f4
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-13 21:48:02 -0500

    feat(main): added lazy loading route for main view component.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <pv-toast></pv-toast>
+  <header>
+    <pv-toolbar class="bg-primary" fixed>
+      <template #start>
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
+        <h3>ACME Learning Center</h3>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer"/>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  title: 'ACME Learning Center',
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { label: 'Home', to: '/home' },
+        { label: 'About', to: '/about' },
+        { label: 'Tutorials', to: '/tutorials' }
+      ]
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+
+
+}
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>

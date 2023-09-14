68d1093c28087ae851849589b95676f501504666
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-13 21:29:38 -0500

    feat(routing): added lazy loading route for tutorial list component.

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,15 @@ import HomeComponent from "../public/pages/home.component.vue";
 
 const AboutComponent = () => import('../public/pages/about.component.vue');
 
+const TutorialListComponent = () => import('../learning/pages/tutorial-list.component.vue');
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/home',    component: HomeComponent },
         { path: '/',        redirect: '/home' },
-        { path: '/about',   component: AboutComponent }
+        { path: '/about',   component: AboutComponent },
+        { path: '/tutorials',   component: TutorialListComponent },
     ]
 });
 
